2b0c83c7a95edb0e8709fade2b03e616160a5e7e
tuna1025 <90132409+tuna1025@users.noreply.github.com>
2021-11-27 12:35:09 +0900

    Add files via upload

---
+++ hw8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int len = 50;
+    char str[50];//len이 안들어가지네
+    int i;
+    printf("Input> ");
+    fgets(str, sizeof(str), stdin);
+    for (i = 0; i < len; i++)
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] = str[i] + 32;
+        }
+        else if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;
+        }
+    }
+
+    printf("Output> %s\n", str);
+
+
+
+    return 0;
+}
